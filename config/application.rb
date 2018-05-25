a710e7cf60e3e5071858196b1cf50449942ed2d7
thekernel <salut.amos@gmail.com>
2018-05-25 14:33:55 +0000

    activate french as default language

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Acres
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :fr
+
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
