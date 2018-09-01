fe66adf5c64763a15f7680074b652a2d8fbf7604
thekernel <salut.amos@gmail.com>
2018-09-01 22:36:51 +0000

    edit deploy server host address

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "https://github.com/thkernel/acres.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
- set :deploy_to, "/home/openkm/acres"
+ set :deploy_to, "/home/ubuntu/acres-app"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
