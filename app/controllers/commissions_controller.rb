0b1e33ca7e8baa0218ac2efe7f8a31f6d23b270c
thkernel <salut.amos@gmail.com>
2018-09-28 05:29:43 +0000

    fix bugs

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -17,18 +17,24 @@ class CommissionsController < ApplicationController
 	def contributors
 
 		if current_user.present? && is_contributor?(current_user)
-			@commissions = Commission.where(contributor_name: current_user.full_name)
+			@commissions = Commission.where(contributor_name: current_user.full_name).where(created_by: get_main_admin(current_user))
 		elsif is_admin?
 			#@commissions = current_user.commissions 
-			users = User.where(role: 'Apporteur')
-			@commissions = []
-			users.each {|user|
-				commissions = Commission.where(contributor_name: user.full_name)
-				commissions.each{|commission|
-					@commissions.push(commission)
+			users = User.where(role: 'Apporteur').where(created_by: get_main_admin(current_user))
+			
+			if users.present?
+				@commissions = []
+				users.each {|user|
+					commissions = Commission.where(contributor_name: user.full_name).where(created_by: get_main_admin(current_user))
+
+					if commissions.present?
+						commissions.each{|commission|
+							@commissions.push(commission)
+						}
+					end
+					
 				}
-				
-			}
+			end
 		end
 
 		
@@ -36,18 +42,23 @@ class CommissionsController < ApplicationController
 
 	def producers
 		if current_user.present? && is_producer?(current_user)
-			@commissions = Commission.where(producer_name: current_user.full_name)
+			@commissions = Commission.where(producer_name: current_user.full_name).where(created_by: get_main_admin(current_user))
 		elsif is_admin?
 
 			#@commissions = current_user.commissions 
-			users = User.where(role: 'Producteur')
-			@commissions = []
-			users.each do |user|
-				commissions = Commission.where(producer_name: user.full_name)
-				commissions.each do |commission|
-					@commissions.push(commission)
+			users = User.where(role: 'Producteur').where(created_by: get_main_admin(current_user))
+			
+			if users.present?
+				@commissions = []
+				users.each do |user|
+					commissions = Commission.where(producer_name: user.full_name).where(created_by: get_main_admin(current_user))
+					if comissions.present?
+						commissions.each do |commission|
+							@commissions.push(commission)
+						end
+					end
+					
 				end
-				
 			end
 		end
 	end
