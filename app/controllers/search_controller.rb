464bb6db6821d5c2f50e4c550e08d4d288ec6239
thkernel <salut.amos@gmail.com>
2018-10-23 05:58:32 +0000

    update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -24,13 +24,16 @@ class SearchController < ApplicationController
     producer_name = params[:producer] if params[:producer].present?
     contributor_name = params[:contributor] if params[:contributor].present?
     
-    puts "Je suis une date avant #{params[:production_date]}"
+    #puts "Je suis une date avant #{params[:production_date_debut]}"
     
-    production_date = Date.parse(params[:production_date]) if params[:production_date].present?
+    production_date_debut = Date.parse(params[:production_date_debut]) if params[:production_date_debut].present?
+    production_date_fin = Date.parse(params[:production_date_fin]) if params[:production_date_fin].present?
+
+    #puts "Je suis une #{production_date}"
     
-    puts "Je suis une #{production_date}"
-    
-    acte_date = Date.parse(params[:acte_date]) if params[:acte_date].present? 
+    acte_date_debut = Date.parse(params[:acte_date_debut]) if params[:acte_date_debut].present? 
+    acte_date_fin = Date.parse(params[:acte_date_fin]) if params[:acte_date_fin].present? 
+
     
     notary = params[:notary] if params[:notary].present?
 
@@ -39,7 +42,7 @@ class SearchController < ApplicationController
     contributor_name = contributor_name.reject{ |e| e.to_s.empty? } if contributor_name.present?
 
 
-    @commissions = Commission.search(production_date,acte_date,  bank_name, producer_name, contributor_name, notary, get_main_admin(current_user))#.paginate(:page => params[:page], :per_page => 15) #if Credit.search(bank_name).present?
+    @commissions = Commission.search(production_date_debut,production_date_fin, acte_date_debut, acte_date_fin,   bank_name, producer_name, contributor_name, notary, get_main_admin(current_user))#.paginate(:page => params[:page], :per_page => 15) #if Credit.search(bank_name).present?
 
     respond_to do |format|
       format.html { }
