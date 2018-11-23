b069affea1024543faa06d695971ce20dc5b5c41
thekernel <salut.amos@gmail.com>
2018-11-23 11:35:25 +0000

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,8 @@ set :application, "acres"               # the name of your website - should also
 set :deploy_to, "/home/ubuntu/acres-app"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
 set :rails_env, "production"
 
-server "51.38.235.131", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
+#server "51.38.235.131", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
+server "http://gescomph.be", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
 
 
 
