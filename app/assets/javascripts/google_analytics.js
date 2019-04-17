3d82ebd1bf9614ea1f47a953d679f61ca02ab66d
thkernel <salut.amos@gmail.com>
2019-04-17 14:38:03 +0000

    update

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbolinks:load', event => {
+document.addEventListener('turbolinks:load', function(event) {
     if (typeof gtag === 'function') {
       gtag('config', 'UA-1183577-19', {
         'page_location': event.data.url
